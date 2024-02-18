00c6dd9eeb8c73e23413756de6746bc98da2af90
Bikash Mishra <mishrabikash202@gmail.com>
2024-02-18 14:28:29 +0530

    setup

--- frontend/frontend/compConf.js
+++ frontend/frontend/compConf.js
@@ -1,6 +1,6 @@
-import Transfer from "./src/component/Transfer";
-import Signin from "./src/component/Signin";
-import Signup from "./src/component/Signup";
-import Dashboard from "./src/component/Dashboard";
+import Transfer from "./src/pages/Transfer";
+import Signin from "./src/pages/Signin";
+import Signup from "./src/pages/Signup";
+import Dashboard from "./src/pages/Dashboard";
 
 export { Transfer, Signin, Signup, Dashboard };
